2bf00c348b3dd36bd9d0ae66266ae5d07f628d80
Xin Shi <xshi@orca4>
2014-03-04 14:19:36 -0500

    first working version

--- py/csv2xml.py
+++ py/csv2xml.py
@@ -15,15 +15,32 @@ xmlFile = csvFile.replace('.csv', '.xml')
 
 csvData = csv.reader(open(csvFile))
 xmlData = open(xmlFile, 'w')
-#xmlData.write('<?xml version="1.0"?>' + "\n")
+
+xmlHeader = '''<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
+<ROOT xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <HEADER>
+    <TYPE>
+      <EXTENSION_TABLE_NAME>UPGRADE_FPIX_SNSRWAFR_IV</EXTENSION_TABLE_NAME>
+      <NAME></NAME>
+    </TYPE>
+    <RUN>
+      <RUN_NAME></RUN_NAME>
+      <RUN_BEGIN_TIMESTAMP></RUN_BEGIN_TIMESTAMP>
+      <INITIATED_BY_USER></INITIATED_BY_USER>
+      <LOCATION>Purdue</LOCATION>
+      <COMMENT_DESCRIPTION></COMMENT_DESCRIPTION>
+    </RUN>
+  </HEADER>
+'''
+
+xmlData.write(xmlHeader)
+xmlData.write('<DATA_SET>\n')
 
 rowNum = 0
 for row in csvData:
     rowNum +=1
 
     if rowNum == 1:
-        #print row # title 
-        # need to add header here 
         print 'Using %s and %s as Voltage and Current ...' %(row[0], row[1]) 
 
     else: 
@@ -37,8 +54,10 @@ for row in csvData:
         if len(current) == 0:
             break
 
-
+xmlData.write('</DATA_SET>\n')
+xmlData.write('</ROOT>\n')
 xmlData.close()
+
 print 'Saved as %s.' %xmlFile
 
 
