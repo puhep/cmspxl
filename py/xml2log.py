17122e96fd71de54b500b84747918a192b05f96d
Xin Shi <xshi@orca4>
2014-05-17 15:17:50 -0400

    wildcard

--- py/xml2log.py
+++ py/xml2log.py
@@ -7,26 +7,35 @@ Translate the XML to log format
 __author__ = "Xin Shi <Xin.Shi@cern.ch>"
 
 import sys
-import xml.etree.ElementTree as ET
-
-xmlFile = sys.argv[1] 
-logFile = xmlFile.replace('.xml', '.log')
-
-tree = ET.parse(xmlFile)
-root = tree.getroot()
-
-Vs = [ data.text for data in root.iter('VOLTAGE_VOLT')]
-Is =[ data.text for data in root.iter('ACTV_CURRENT_AMP')]
-
-IVs = zip(Vs, Is)
-
-logData = open(logFile, 'w')
-for IV in IVs:
-    logData.write('%s  %s \n' %IV)
-
-print 'Saved as %s.' %logFile
+import xml.etree.ElementTree as et
 
 
 
+def xml2log(xmlFile):
+    tree = et.parse(xmlFile)
+    root = tree.getroot()
+
+    Vs = [ data.text for data in root.iter('VOLTAGE_VOLT')]
+    Is =[ data.text for data in root.iter('ACTV_CURRENT_AMP')]
+
+    IVs = zip(Vs, Is)
+
+    logFile = xmlFile.replace('.xml', '.log')    
+    logData = open(logFile, 'w')
+    for IV in IVs:
+        logData.write('%s  %s \n' %IV)
+
+    print 'Saved as %s.' %logFile
+
+xmlFiles = sys.argv[1:]
+
+for xmlFile in xmlFiles:
+    xml2log(xmlFile)
+
+
+
+
+
+    
 
 
