8d6fbaefb8378686dc3ce1356e3c4c1633103a71
Xin Shi <xshi@orca4>
2014-06-06 17:18:18 -0400

    save hist

--- src/drawMod.cc
+++ src/drawMod.cc
@@ -134,10 +134,10 @@ TCanvas* drawMod(TString label, TString inputFile, int V=0){
   gStyle->SetOptStat(0);
   gStyle->SetTitle(0);
 
-  // TFile *f = new TFile("h_mod.root", "RECREATE");
-  // h3->Write();
-  // f->Close();
-  // cout << "Save the hist as: h_mod.root" << endl;  
+  TFile *f = new TFile("h_mod.root", "RECREATE");
+  h3->Write();
+  f->Close();
+  cout << "Save the hist as: h_mod.root" << endl;  
 
   return c; 
 }
