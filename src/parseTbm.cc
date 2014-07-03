6593bc2e8f4f39184959cd2eb93ebbf0a9546827
Xin Shi <xshi@orca4>
2014-07-03 15:59:39 -0400

    start

---
+++ src/parseTbm.cc
@@ -0,0 +1,28 @@
+//
+// Parse the output from TBM 
+// Author: Xin Shi <Xin.Shi@cern.ch> 
+// 2014.07.03
+// 
+
+
+#include <iostream>
+#include <limits>  
+
+using namespace std; 
+
+void print_usage(){
+  cerr << "Usage: parseTbm inputFile" << endl; 
+}
+
+
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    print_usage() ;  
+    return -1; 
+  }
+  
+  
+
+}
+
+
