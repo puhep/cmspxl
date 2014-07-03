210e97587fe912583ca23db6f511ababfd89e51b
Xin Shi <xshi@orca4>
2014-07-03 16:51:08 -0400

    stop here

--- py/tbmparser.py
+++ py/tbmparser.py
@@ -24,10 +24,11 @@ def main():
         return usage()
 
     filename = args[0]
-    fi = open(filename, 'r')
-    for line in fi:
-        print line
-        
+
+    tbm = TBMFile(filename)
+    
+    print tbm.data
+
     sys.exit()
 
     if args[0] == 'dac':
@@ -38,65 +39,39 @@ def main():
         raise NameError(args)
 
 
-def dac(args):
-    rocs = get_rocs(args[0])
-    dacpath = os.getcwd()
-    for roc in rocs:
-        inname = 'dacParameters_C%s.dat' %roc
-        infile = os.path.join(dacpath, inname)
-        dac = UserFile(infile)
-        action = args[1]
-        if action == 'set':
-            key = args[2]
-            value = args[3]
-            dac.set_key_value(key, value, sep=' ', 
-                              key_pos=1, value_pos=2, verbose=1)
 
-        elif action == 'add':
-            key = args[2]
-            value = args[3]
-            dac.add_key_value(key, value, sep=' ', 
-                              key_pos=1, value_pos=2, verbose=1)
-                        
-        elif action == 'sub':
-            key = args[2]
-            value = args[3]
-            dac.sub_key_value(key, value, sep=' ',
-                              key_pos=1, value_pos=2, verbose=1)
-                        
-        else:
-            raise NameError(args[1])
-        dac.output(infile)
+class TBMFile(object):
+    '''Class to handle TBM output file  '''
+    def __init__(self, filename=None):
+        self.data = []        
+        if filename:
+            self.input(filename)
+            self.file = filename
+
+        if self.data:
+            self.parse()
+            
+        
+    def input(self, filename, verbose=0):
+        fi = open(filename, 'r')
+        for line in fi:
+            self.data.append(line)
+        fi.close()
+
+    def parse(self):
+        n_decode = 0
+        n_coreA = 0
+        n_coreB = 0
+
+        for dt in self.data:
+            if 'words' in dt:
+                n_decode += 1
+                
+                
+            print dt
+            sys.exit()
 
     
-def board(args):
-    cfgpath = os.getcwd()
-    cfgname = 'boardConfigurationDataTaking.txt'
-    cfgfile = os.path.join(cfgpath, cfgname)
-    board = UserFile(cfgfile)
-
-    action = args[0]
-    if action == 'set':
-        key = args[1]
-        value = args[2]
-        board.set_key_value(key, value, sep=' ', verbose=1)
-    else:
-        raise NameError(args[1])
-
-    new_cfgname = cfgname.replace('.txt', '_%s_%s.txt' %(key, value))
-    new_cfgfile = os.path.join(cfgpath, new_cfgname)
-
-    board.output(new_cfgfile)
-
-
-def get_rocs(args):
-    if args == 'all':
-        rocs = range(16)
-    else:
-        rocs = args.split(',')
-    return rocs 
-    
-
 if __name__ == '__main__':
     main()
 
