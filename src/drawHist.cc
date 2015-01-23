8325f83cfb703de079d53e682a53e10d794d2aea
Purdue HEP <purduehep@gmail.com>
2015-01-23 15:11:26 -0500

    Implemented -vmin to help find empty pixels

--- src/drawHist.cc
+++ src/drawHist.cc
@@ -58,13 +58,14 @@ void set_root_style(int stat=1110, int grid=0){
   gStyle->SetStatBorderSize(1); 
 }
 
-
 TCanvas* drawHist(vector<TString> inputFiles,
 		  TString histType, 
 		  TString histName, 
 		  TString opt, 
 		  double vmax,
-		  int npad, 
+		  double vmin,
+		  bool printpix,
+		  int npad,
 		  int V=0){
 
   int ww, wh;
@@ -96,7 +97,7 @@ TCanvas* drawHist(vector<TString> inputFiles,
   leg->SetTextSize(0.02);
   leg->SetTextSizePixels(25);
 
-  for (vector<int>:: size_type i = 0; i != inputFiles.size(); i++) {
+  for (vector<int>:: size_type i = 0; i != inputFiles.size(); i++) {    
     if (npad > 1) 
       c->cd(i+1); 
 
@@ -142,20 +143,37 @@ TCanvas* drawHist(vector<TString> inputFiles,
       // h->Write();
     }
 
-    else if (!strcmp(histType, "TH2D")) {
+    else if (!strcmp(histType, "TH2D")) {      
       TH2D *h = (TH2D*)f->Get(histName);
       if (!h) {
 	cout << "Not able to find hist: " << histName << endl; 
 	return NULL; 
       }
+      
+      int nbinx = h->GetXaxis()->GetNbins();
+      int nbiny = h->GetYaxis()->GetNbins();
+      double v;
       // h->SetDirectory(0); 
-      // delete f; 
-      h->SetTitle(inputFiles[i]); 
+      // delete f;
+      if (printpix==true) {
+	printf("  Pixel Values:\n");
+        for (int ix = 1; ix <= nbinx; ix++) {
+	  for (int iy = 1; iy <= nbiny; iy++) {
+	    v = h->GetBinContent(ix, iy);
+	    if (v>=vmin && v<=vmax) {
+	      printf("\tc%i, r%i: %f \n", ix, iy, v);
+	    }
+	  }
+        }
+      }
+      h->SetTitle(inputFiles[i]);
       if ( vmax != numeric_limits<double>::max())
-	h->SetMaximum(vmax); 
+	h->SetMaximum(vmax);
+      if (vmin != -numeric_limits<double>::max())
+	h->SetMinimum(vmin);
       if (opt == "") h->Draw("colz"); 
       else h->Draw(opt);
-      h->Write();
+      //h->Write();
     }
   
     else {
@@ -169,10 +187,10 @@ TCanvas* drawHist(vector<TString> inputFiles,
   // gROOT->SetStyle("Plain");
   // gStyle->SetOptStat(0);
 
-  TFile *fo = new TFile(h_file_name, "RECREATE");
-  h->Write();
-  fo->Close();
-  cout << "Save the hist as: "<< h_file_name << endl;  
+  //TFile *fo = new TFile(h_file_name, "RECREATE");
+  //h->Write();
+  //fo->Close();
+  //cout << "Save the hist as: "<< h_file_name << endl;  
   
   return c; 
 }
@@ -181,13 +199,15 @@ TCanvas* drawHist(vector<TString> inputFiles,
 void print_usage(){
   printf("NAME\n\tdrawHist - draw Histograms\n");
   printf("\nSYNOPSIS\n\tdrawHist [-t hist-type ] [-opt draw-option]\n "); 
-  printf("\t[-h hist-name ] [-vmax max-value] [-npad num-pad] input1 input2 ...\n");
+  printf("\t[-h hist-name ] [-vmax max-value] [-npad num-pad] [-b] input1 input2 ...\n");
   printf("\nOPTIONS\n");
+  printf("\t%-5s  %-40s\n", "-b", "batch mode");
   printf("\t%-5s  %-40s\n", "-t", "hist type [TH1D, TH2D]");
   printf("\n\t%-5s  %-40s\n", "-opt", "draw option for histgram [colz, surf2]");
   printf("\n\t%-5s  %-40s\n", "-h", "histogram name");
   printf("\n\t%-5s  %-40s\n", "-vmax", "limit the histogram within vmax-value");
-  printf("\n\t%-5s  %-40s\n", "-npad", "number of pads, set to 1 for superimpose");
+  printf("\n\t%-5s  %-40s\n", "-vmin", "limit the histogram within vmin-value");
+  printf("\n\t%-5s  %-40s\n", "-p", "print pixel values to stdout");
   printf("\nAUTHOR\n\tXin Shi <Xin.Shi@cern.ch>\n");
 }
 
@@ -205,16 +225,18 @@ int main(int argc, char** argv) {
   TString histName; 
   TString opt(""); 
   double vmax = numeric_limits<double>::max();
-  int npad = 1; // use 1 as default  
+  double vmin = -numeric_limits<double>::max();
+  bool printpix(false);
+  int npad = 1; // use 1 as default
 
   int delta_idx = 0; 
   for (int i = 0; i < argc; i++){
-    // if (!strcmp(argv[i],"-b")) {
-    //   doRunGui = false;  
-    //   inputFiles.erase(inputFiles.begin()+i-2+delta_idx,
-    // 		       inputFiles.begin()+i+delta_idx); 
-    //   delta_idx -= 1; 
-    // } 
+    if (!strcmp(argv[i],"-b")) {
+      doRunGui = false;  
+      inputFiles.erase(inputFiles.begin()+i-2+delta_idx,
+    		       inputFiles.begin()+i+delta_idx); 
+      delta_idx -= 1; 
+    } 
 
     if (!strcmp(argv[i],"-t")) {
       histType = string(argv[++i]); 
@@ -246,6 +268,21 @@ int main(int argc, char** argv) {
       cout << "Using vmax = " << vmax << endl; 
     }
     
+    if (!strcmp(argv[i],"-vmin")) {
+      vmin = atof(argv[++i]); 
+      inputFiles.erase(inputFiles.begin()+i-2+delta_idx,
+    		       inputFiles.begin()+i+delta_idx); 
+      delta_idx -= 2; 
+      cout << "Using vmin = " << vmin << endl;
+    }
+
+    if (!strcmp(argv[i],"-p")) {
+      printpix = true;
+      inputFiles.erase(inputFiles.begin()+i-1+delta_idx,
+      	       inputFiles.begin()+i+delta_idx); 
+      delta_idx -= 1;
+    }
+    
     if (!strcmp(argv[i],"-npad")) {
       npad = atof(argv[++i]); 
       inputFiles.erase(inputFiles.begin()+i-2+delta_idx,
@@ -253,7 +290,6 @@ int main(int argc, char** argv) {
       delta_idx -= 2; 
       cout << "Using npad = " << npad << endl; 
     }
- 
   }
   
   for (vector<int>:: size_type i = 0; i != inputFiles.size(); i++) {
@@ -264,17 +300,18 @@ int main(int argc, char** argv) {
   if (doRunGui) { 
     TApplication theApp("App", 0, 0);
     theApp.SetReturnFromRun(true);
-    drawHist(inputFiles, histType, histName, opt, vmax, npad);  
+    drawHist(inputFiles, histType, histName, opt, vmax, vmin, printpix, npad);  
     theApp.Run();
   } 
   
   else {
     TCanvas *c = drawHist(inputFiles, histType, histName, opt, 
-			  vmax, npad);  
-    TString outFile = inputFile;
-    outFile.ReplaceAll("root",4,"pdf",3);  
-    c->SaveAs(outFile);
-    outFile.ReplaceAll("pdf",3,"png",3);  
+			  vmax, vmin, printpix, npad);  
+    TString outFile = "h_test.pdf";
+    // TString outFile = inputFile;
+    // outFile.ReplaceAll("root",4,"pdf",3);  
+    // c->SaveAs(outFile);
+    // outFile.ReplaceAll("pdf",3,"png",3);  
     c->SaveAs(outFile);
     delete c;
     gSystem->Exit(0);
