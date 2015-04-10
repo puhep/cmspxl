15bc000db8fadc14b04c8d4fd7f345b583ffe172
Purdue HEP <purduehep@gmail.com>
2015-04-10 16:51:51 -0400

    update setpxl 1.1

--- setup.sh
+++ setup.sh
@@ -187,6 +187,11 @@ setpxl() {
 	return 
     fi; 
 
+    if [ "$1" = 1.1 ]; then
+	setroot5 
+	export PATH=/home/purduepix/cmspxl/v2015.04/bin:/home/purduepix/pxar/pxar_crosscalibrate/bin:/home/purduepix/pxar/pxar_crosscalibrate/main:/home/purduepix/elComandante/v1.0/subsystem:/home/purduepix/elComandante/v1.0/elComandante:$PATH 
+    fi;
+
     if [ "$1" = 1.0 ]; then
 	setroot5 
 	export PATH=/home/purduepix/cmspxl/v2015.01/bin:/home/purduepix/pxar/v2014.11/bin:/home/purduepix/pxar/v2014.11/main:/home/purduepix/elComandante/v1.0/subsystem:/home/purduepix/elComandante/v1.0/elComandante:$PATH 
@@ -204,7 +209,7 @@ setpxl() {
 }
 
 # Set the default env 
-setpxl 1.0 
+setpxl 1.1 
 
 
 
