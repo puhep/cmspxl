10248d90d333a583c8e1c10877ce3b116f24ad84
Purdue HEP <purduehep@gmail.com>
2015-05-07 15:14:14 -0400

    remove unused files

--- www/Submission_p/maps/CV_Map.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Content-disposition: attachment; filename=CV_Map.xsd');
-header('Content-type: application/xml');
-readfile('CV_Map.xsd');
-?>
--- www/Submission_p/maps/IV_Map.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Content-disposition: attachment; filename=IV_Map.xsd');
-header('Content-type: application/xml');
-readfile('IV_Map.xsd');
-?>
